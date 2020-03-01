b5347dcc304f9455df2880a4ac27735f3ff4bce1
dellingercl2 <46876230+dellingercl2@users.noreply.github.com>
2020-02-29 20:47:02 -0500

    Update CVControl.py

--- CVControl.py
+++ CVControl.py
@@ -2,29 +2,94 @@ import cv2
 import numpy as np
 import pyautogui as pag
 
+hand_hist = None
+traverse_point = []
+total_rectangle = 9
+hand_rect_one_x = None
+hand_rect_one_y = None
 
+hand_rect_two_x = None
+hand_rect_two_y = None
 
 cap = cv2.VideoCapture(0)
 bg = cv2.createBackgroundSubtractorMOG2(history = 100, 
                                         varThreshold = 15, 
                                         detectShadows = False)
 
+def draw_rect(frame):
+    rows, cols, _ = frame.shape
+    global total_rectangle, hand_rect_one_x, hand_rect_one_y, hand_rect_two_x, hand_rect_two_y
+
+    hand_rect_one_x = np.array(
+        [6 * rows / 20, 6 * rows / 20, 6 * rows / 20, 9 * rows / 20, 9 * rows / 20, 9 * rows / 20, 12 * rows / 20,
+         12 * rows / 20, 12 * rows / 20], dtype=np.uint32)
+
+    hand_rect_one_y = np.array(
+        [9 * cols / 20, 10 * cols / 20, 11 * cols / 20, 9 * cols / 20, 10 * cols / 20, 11 * cols / 20, 9 * cols / 20,
+         10 * cols / 20, 11 * cols / 20], dtype=np.uint32)
+
+    hand_rect_two_x = hand_rect_one_x + 25
+    hand_rect_two_y = hand_rect_one_y + 25
+
+    for i in range(total_rectangle):
+        cv2.rectangle(frame, (hand_rect_one_y[i], hand_rect_one_x[i]),
+                      (hand_rect_two_y[i], hand_rect_two_x[i]),
+                      (0, 255, 0), 1)
+    return frame
+
+def hand_histogram(frame):
+    global hand_rect_one_x, hand_rect_one_y
+
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    roi = np.zeros([90, 10, 3], dtype=hsv_frame.dtype)
+
+    for i in range(total_rectangle):
+        roi[i * 10: i * 10 + 10, 0: 10] = hsv_frame[hand_rect_one_x[i]:hand_rect_one_x[i] + 10,
+                                          hand_rect_one_y[i]:hand_rect_one_y[i] + 10]
+
+    hand_hist = cv2.calcHist([roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
+    print("Hand histogram created")
+    return cv2.normalize(hand_hist, hand_hist, 0, 255, cv2.NORM_MINMAX)
+    
+    
+
+def hist_masking(frame, hist):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    dst = cv2.calcBackProject([hsv], [0, 1], hist, [0, 180, 0, 256], 1)
+
+    disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (31, 31))
+    cv2.filter2D(dst, -1, disc, dst)
+
+    ret, thresh = cv2.threshold(dst, 150, 255, cv2.THRESH_BINARY)
+
+    thresh = cv2.merge((thresh, thresh, thresh))
+
+    return cv2.bitwise_and(frame, thresh)
 
 
 if not cap.isOpened():
     raise IOError("Cannot open webcam")
 
+hist_created = False
 
-while True:
+while cap.isOpened():
     ret, frame = cap.read()
+    pressed_key = cv2.waitKey(1)
     frame = cv2.resize(frame, (1024, 768))
     frame = cv2.flip(frame, 1)
+        
+    if pressed_key & 0xFF == ord('z'):
+        hist_created = True
+        hist = hand_histogram(frame)
     
-  
-       
+    if hist_created:
+        pass
+    else:
+        frame = draw_rect(frame)
+   
     cv2.imshow('Frame', frame )
-    c = cv2.waitKey(1)
-    if c == 27:
+    
+    if pressed_key == 27:
         break
 cap.release()
 cv2.destroyAllWindows()
