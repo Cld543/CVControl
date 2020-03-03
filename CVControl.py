120ca6a53c52df038e5bf3211836c226a9b291ef
dellingercl2 <46876230+dellingercl2@users.noreply.github.com>
2020-03-03 13:50:27 -0500

    Update CVControl.py

--- CVControl.py
+++ CVControl.py
@@ -1,95 +1,139 @@
 import cv2
 import numpy as np
-import pyautogui as pag
+import copy
+import math
+#from appscript import app
 
-hand_hist = None
-traverse_point = []
-total_rectangle = 9
-hand_rect_one_x = None
-hand_rect_one_y = None
+# Environment:
+# OS    : Mac OS EL Capitan
+# python: 3.5
+# opencv: 2.4.13
 
-hand_rect_two_x = None
-hand_rect_two_y = None
+# parameters
+cap_region_x_begin=0.5  # start point/total width
+cap_region_y_end=0.8  # start point/total width
+threshold = 60  #  BINARY threshold
+blurValue = 41  # GaussianBlur parameter
+bgSubThreshold = 50
+learningRate = 0
 
-cap = cv2.VideoCapture(0)
-bg = cv2.createBackgroundSubtractorMOG2(history = 100, 
-                                        varThreshold = 15, 
-                                        detectShadows = False)
+# variables
+isBgCaptured = 0   # bool, whether the background captured
+triggerSwitch = False  # if true, keyborad simulator works
 
-def draw_rect(frame):
-    rows, cols, _ = frame.shape
-    global total_rectangle, hand_rect_one_x, hand_rect_one_y, hand_rect_two_x, hand_rect_two_y
-
-    hand_rect_one_x = np.array(
-        [6 * rows / 20, 6 * rows / 20, 6 * rows / 20, 9 * rows / 20, 9 * rows / 20, 9 * rows / 20, 12 * rows / 20,
-         12 * rows / 20, 12 * rows / 20], dtype=np.uint32)
-
-    hand_rect_one_y = np.array(
-        [9 * cols / 20, 10 * cols / 20, 11 * cols / 20, 9 * cols / 20, 10 * cols / 20, 11 * cols / 20, 9 * cols / 20,
-         10 * cols / 20, 11 * cols / 20], dtype=np.uint32)
-
-    hand_rect_two_x = hand_rect_one_x + 25
-    hand_rect_two_y = hand_rect_one_y + 25
-
-    for i in range(total_rectangle):
-        cv2.rectangle(frame, (hand_rect_one_y[i], hand_rect_one_x[i]),
-                      (hand_rect_two_y[i], hand_rect_two_x[i]),
-                      (0, 255, 0), 1)
-    return frame
-
-def hand_histogram(frame):
-    global hand_rect_one_x, hand_rect_one_y
-
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    roi = np.zeros([90, 10, 3], dtype=hsv_frame.dtype)
-
-    for i in range(total_rectangle):
-        roi[i * 10: i * 10 + 10, 0: 10] = hsv_frame[hand_rect_one_x[i]:hand_rect_one_x[i] + 10,
-                                          hand_rect_one_y[i]:hand_rect_one_y[i] + 10]
-
-    hand_hist = cv2.calcHist([roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
-    print("Hand histogram created")
-    return cv2.normalize(hand_hist, hand_hist, 0, 255, cv2.NORM_MINMAX)
-    
-    
-
-def mask_hist(frame, hist):
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    dst = cv2.calcBackProject([hsv], [0, 1], hist, [0, 180, 0, 256], 1)
-
-    disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (31, 31))
-    cv2.filter2D(dst, -1, disc, dst)
-
-    ret, thresh = cv2.threshold(dst, 150, 255, cv2.THRESH_BINARY)
-
-    thresh = cv2.merge((thresh, thresh, thresh))
-
-    return cv2.bitwise_and(frame, thresh)
+def printThreshold(thr):
+    print("! Changed threshold to "+str(thr))
 
 
-if not cap.isOpened():
-    raise IOError("Cannot open webcam")
+def removeBG(frame):
+    fgmask = bgModel.apply(frame,learningRate=learningRate)
+    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    # res = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 
-hist_created = False
+    kernel = np.ones((3, 3), np.uint8)
+    fgmask = cv2.erode(fgmask, kernel, iterations=1)
+    res = cv2.bitwise_and(frame, frame, mask=fgmask)
+    return res
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    pressed_key = cv2.waitKey(1)
-    frame = cv2.resize(frame, (1024, 768))
-    frame = cv2.flip(frame, 1)
-        
-    if pressed_key & 0xFF == ord(' '):
-        hist_created = True
-        hist = hand_histogram(frame)
-    
-    if hist_created:
-        frame = mask_hist(frame, hist)
-    else:
-        frame = draw_rect(frame)
-   
-    cv2.imshow('Frame', frame )
-    
-    if pressed_key == 27:
+
+def calculateFingers(res,drawing):  # -> finished bool, cnt: finger count
+    #  convexity defect
+    hull = cv2.convexHull(res, returnPoints=False)
+    if len(hull) > 3:
+        defects = cv2.convexityDefects(res, hull)
+        if type(defects) != type(None):  # avoid crashing.   (BUG not found)
+
+            cnt = 0
+            for i in range(defects.shape[0]):  # calculate the angle
+                s, e, f, d = defects[i][0]
+                start = tuple(res[s][0])
+                end = tuple(res[e][0])
+                far = tuple(res[f][0])
+                a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+                b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+                c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+                angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  # cosine theorem
+                if angle <= math.pi / 2:  # angle less than 90 degree, treat as fingers
+                    cnt += 1
+                    cv2.circle(drawing, far, 8, [211, 84, 0], -1)
+            return True, cnt
+    return False, 0
+
+
+# Camera
+camera = cv2.VideoCapture(0)
+camera.set(10,200)
+cv2.namedWindow('trackbar')
+cv2.createTrackbar('trh1', 'trackbar', threshold, 100, printThreshold)
+
+
+while camera.isOpened():
+    ret, frame = camera.read()
+    threshold = cv2.getTrackbarPos('trh1', 'trackbar')
+    frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
+    frame = cv2.flip(frame, 1)  # flip the frame horizontally
+    cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
+                 (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
+    cv2.imshow('original', frame)
+
+    #  Main operation
+    if isBgCaptured == 1:  # this part wont run until background captured
+        img = removeBG(frame)
+        img = img[0:int(cap_region_y_end * frame.shape[0]),
+                    int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]  # clip the ROI
+        cv2.imshow('mask', img)
+
+        # convert the image into binary image
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0)
+        cv2.imshow('blur', blur)
+        ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY)
+        cv2.imshow('ori', thresh)
+
+
+        # get the coutours
+        thresh1 = copy.deepcopy(thresh)
+        contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        length = len(contours)
+        maxArea = -1
+        if length > 0:
+            for i in range(length):  # find the biggest contour (according to area)
+                temp = contours[i]
+                area = cv2.contourArea(temp)
+                if area > maxArea:
+                    maxArea = area
+                    ci = i
+
+            res = contours[ci]
+            hull = cv2.convexHull(res)
+            drawing = np.zeros(img.shape, np.uint8)
+            cv2.drawContours(drawing, [res], 0, (0, 255, 0), 2)
+            cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 3)
+
+            isFinishCal,cnt = calculateFingers(res,drawing)
+            if triggerSwitch is True:
+                if isFinishCal is True and cnt <= 2:
+                    print (cnt)
+                    #app('System Events').keystroke(' ')  # simulate pressing blank space
+                    
+
+        cv2.imshow('output', drawing)
+
+    # Keyboard OP
+    k = cv2.waitKey(10)
+    if k == 27:  # press ESC to exit
+        camera.release()
+        cv2.destroyAllWindows()
         break
-cap.release()
-cv2.destroyAllWindows()
+    elif k == ord('b'):  # press 'b' to capture the background
+        bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
+        isBgCaptured = 1
+        print( '!!!Background Captured!!!')
+    elif k == ord('r'):  # press 'r' to reset the background
+        bgModel = None
+        triggerSwitch = False
+        isBgCaptured = 0
+        print ('!!!Reset BackGround!!!')
+    elif k == ord('n'):
+        triggerSwitch = True
+        print ('!!!Trigger On!!!')
