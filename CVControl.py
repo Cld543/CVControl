66fde80f95f87d34b8aedbb4e8604789f6f3493a
dellingercl2 <46876230+dellingercl2@users.noreply.github.com>
2020-03-28 17:44:00 -0400

    Update CVControl.py

--- CVControl.py
+++ CVControl.py
@@ -1,18 +1,28 @@
 import cv2
 import numpy as np
+import copy
 
 bg_captured = False # Flag to determine if background has been captured.
 cap = cv2.VideoCapture(0)
+cap.set(10, 200)
 bg = None # The captured background image to subtract
-bg_region_x = 0.3
-bg_region_y = 0.9
+bg_region_x = 0.5
+bg_region_y = 0.7
 
 def subtract_bg(frame):
     fgmask = bg.apply(frame, learningRate=0)
     kernel = np.ones((3, 3), np.uint8)
-    fgmask = cv2.erode(fgmask, kernel, iterations = 1)
+    #fgmask = cv2.erode(fgmask, kernel, iterations = 1)
     image = cv2.bitwise_and(frame, frame, mask=fgmask)
     return image
+
+def get_center(contour):
+    moments = cv2.moments(contour)
+    if moments['m00'] != 0:
+            cx = int(moments['m10']/moments['m00'])
+            cy = int(moments['m01']/moments['m00'])
+            return cx, cy
+
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -32,9 +42,41 @@ while cap.isOpened():
                     int(bg_region_x * frame.shape[1]):frame.shape[1]]
         
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("Grar", gray)
         blur = cv2.GaussianBlur(gray, (41, 41), 0)
+        cv2.imshow("Blue", blur)
         ret, thresh = cv2.threshold(blur, 60, 255, cv2.THRESH_BINARY)
         cv2.imshow("Mask", thresh)
+        
+        thresh2 = copy.deepcopy(thresh)
+        contours, hier = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contour_length = len(contours)
+        max_area = -1
+        
+        if contour_length > 0:
+            for i in range(contour_length):
+                con = contours[i]
+                con_area = cv2.contourArea(con)
+                if con_area > max_area:
+                    max_area = con_area
+                    max_index = i
+            result = contours[max_index] # Get the max contour
+           # hull = cv2.convexHull(result)
+            
+            # Create a blank image to display contours
+            drawing = np.zeros(image.shape, np.uint8)
+           
+            for c in contours:
+                hull = cv2.convexHull(c)
+                cv2.drawContours(drawing, [result], 0, (0, 255, 0), 2)
+                cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 3)
+            
+            cv2.resize(drawing, (1920, 1080))
+            centroid = get_center(result)
+            #print(centroid)
+            cv2.circle(drawing, centroid, 7, (0, 255, 0), -1)
+            cv2.imshow("Contours", drawing)
+            
     # Exit if the Escape key is pressed
     k = cv2.waitKey(10)
     
